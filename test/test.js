ed95e215d86130d4b9e6ee90d27e1f4c98e5de6c
obara.takao <obara.takao@dena.jp>
2014-09-19 02:56:08 +0900

    add isFont, isImage

--- test/test.js
+++ test/test.js
@@ -13,6 +13,8 @@ return new Test("WMMimeType", {
         both:       true, // test the primary module and secondary module
     }).add([
         testWMMimeType_isText,
+        testWMMimeType_isFont,
+        testWMMimeType_isImage,
         testWMMimeType_isAudio,
         testWMMimeType_isVideo,
         testWMMimeType_getMimeType,
@@ -24,6 +26,30 @@ function testWMMimeType_isText(test, pass, miss) {
          WMMimeType.isText("html") ||
          WMMimeType.isText(".html") ||
          WMMimeType.isText(".HTML") ) {
+        test.done(pass());
+    } else {
+        test.done(miss());
+    }
+}
+
+function testWMMimeType_isFont(test, pass, miss) {
+    if ( WMMimeType.isFont("ttf") ||
+         WMMimeType.isFont("otf") ||
+         WMMimeType.isFont("woff") ||
+         WMMimeType.isFont(".woff") ||
+         WMMimeType.isFont(".WOFF") ) {
+        test.done(pass());
+    } else {
+        test.done(miss());
+    }
+}
+
+function testWMMimeType_isImage(test, pass, miss) {
+    if ( WMMimeType.isImage("gif") ||
+         WMMimeType.isImage("png") ||
+         WMMimeType.isImage("jpeg") ||
+         WMMimeType.isImage(".jpg") ||
+         WMMimeType.isImage(".JPEG") ) {
         test.done(pass());
     } else {
         test.done(miss());
